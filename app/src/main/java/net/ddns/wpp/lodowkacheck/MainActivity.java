f8536b259f3791d863223c90ebcde0da4850fc8a
Wojciech Paja <pajawojciech@gmail.com>
2016-04-22 23:15:06 +0200

    Przełączanie przycisków

--- app/src/main/java/net/ddns/wpp/lodowkacheck/MainActivity.java
+++ app/src/main/java/net/ddns/wpp/lodowkacheck/MainActivity.java
@@ -1,15 +1,33 @@
 package net.ddns.wpp.lodowkacheck;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    View.OnClickListener l = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            ButtonX b = (ButtonX)v;
+            b.nextStan();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +37,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Nie zapisano :P", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        int ilosc = 20;
+        ButtonX tab[] = new ButtonX[ilosc];
+
+        for(int i = 0; i < ilosc; i++)
+        {
+            tab[i] = createButton(i);
+            if(i % 3 == 0)
+            {
+                tab[i].setBackgroundColor(Color.LTGRAY);
+            }
+        }
+    }
+
+    private ButtonX createButton(int i)
+    {
+        ButtonX myButton = new ButtonX(this, i);
+        myButton.setText("Push Me" + Integer.toString(i));
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.linearCheck);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        ll.addView(myButton, lp);
+
+        myButton.setOnClickListener(l);
+        return myButton;
     }
 
     @Override
@@ -48,5 +93,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class ButtonX extends Button
+{
+    private int ID;
+    int stan = 0;
+
+    ButtonX(Context c, int id)
+    {
+        super(c);
+        ID = id;
+        setBackgroundColor(Color.LTGRAY);
+    }
+
+    void nextStan()
+    {
+        stan = (stan + 1) % 3;
+        switch (stan)
+        {
+            case 0:
+                this.setBackgroundColor(Color.LTGRAY);
+                break;
+            case 1:
+                this.setBackgroundColor(Color.RED);
+                break;
+            case 2:
+                this.setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    public int getId()
+    {
+        return ID;
     }
 }
